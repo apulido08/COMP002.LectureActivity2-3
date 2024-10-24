e945c1ce6af904025285bbb632f27933623e9bc4
adodoCoder <alavropulido@whccd.edu>
2024-10-23 20:45:50 -0700

    exclamation variable

--- script3.js
+++ script3.js
@@ -6,3 +6,9 @@ for(let i = 0; i <= 10; i++){
 for(let i =10; i >= 0; i--){
     console.log(i);
 }
+
+let outputVariable = "";
+for (let i = 0; i <= 10; i++){
+    outputVariable += "!";
+    console.log(outputVariable);
+}
